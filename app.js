6d8ef0508a3e042752fada94e350d74ef66d3957
trustedtech <brian@trusted.tech>
2020-02-28 22:13:40 -0500

    Created essential files and folder structures;  Developed required classes.

---
+++ app.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+
+const managerHTML = require('./templates/genManagerHTML');
+const engineerHTML = require('./templates/genEngineerHTML');
+const internHTML = require('./templates/genInternHTML');
+
+const html = require('./templates/genMainHTML');
+
